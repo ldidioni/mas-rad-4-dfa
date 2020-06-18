feaed5f857c1aab7bf3a4d3112df72bc34ffceae
Di Dionisio Laurent <laurent.didionisio@he-arc.ch>
2020-06-18 20:42:48 +0200

    Add login screen

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,23 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HttpClientModule } from "@angular/common/http";
+import { LoginPageComponent } from './security/login-page/login-page.component';
+import { FormsModule } from '@angular/forms';
+import { DummyPageComponent } from './dummy-page/dummy-page.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginPageComponent,
+    DummyPageComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
